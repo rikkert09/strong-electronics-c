df7bbf8edf6743766c46110ee9f0a569ae5eb486
Joep Buruma <j.buruma@st.hanze.nl>
2017-11-09 13:18:25 +0100

    start integration of scheduler

--- strong-electronics-c/main.c
+++ strong-electronics-c/main.c
@@ -1,29 +1,17 @@
 #include <avr/io.h>
 #define F_CPU 16000000
 
-#include <util/delay.h>
 #include <stdint.h>
-#include <avr/interrupt.h>
 
 #include "rs232.h"
 #include "besturings_eenheid_prot.h"
 #include "adc.h"
 #include "distance.h"
+#include "AVR_TTC_scheduler.h"
 
+#define LIGHT_MOD
 
-void setup(){
-	initalize_control_unit_prot();
-	init_distance();
-	init_adc();
-}
-
-void test_function(uint16_t data){
-	send_reply(ACK_CONNECTION, data);
-}
-
-void test_function_2(uint16_t data){
-	send_reply(RET_SETTING, data);
-}
+uint16_t sensor_hist [2] = {0, 0};
 
 uint16_t get_temperature() {
 	uint8_t adc_value = get_adc_value(0);
@@ -41,23 +29,45 @@ uint16_t get_lux(){
 }
 
 void return_status(uint16_t data){
-	uint16_t distance = measure_distance();
-	send_reply(RET_STATUS, distance);
-	send_short_USART(get_lux(), TRANSMIT_LITTLE_ENDIAN); 
+	//uint16_t distance = measure_distance();
+	uint16_t avg_sensor = (sensor_hist[0] + sensor_hist[1])/2;
+	send_reply(RET_STATUS, (uint16_t)0x1000);
+	send_short_USART(avg_sensor, TRANSMIT_LITTLE_ENDIAN);
+}
+
+void update_status(){
+	sensor_hist[1] = sensor_hist[0];
+#ifdef LIGHT_MOD
+	sensor_hist[0] = get_lux();
+#endif
+#ifdef TEMP_MOD
+	sensor_hist[0] = get_temperature();
+#endif
+}
+
+void setup(){
+	initalize_control_unit_prot();
+	//init_distance();
+	init_adc();
+	
+	register_handler(REQ_STATUS, return_status);
+	
+	SCH_Init_T1();	
+	SCH_Add_Task(handle_comms, 0, 1);
+	#ifdef LIGHT_MOD
+	SCH_Add_Task(update_status, 300, 300);				// if this is a light sensor, update every 30 sec
+	#endif
+	#ifdef TEMP_MOD
+	SCH_Add_Task(update_status, 400, 400);				// if this is a temperature sensor, update every 40 sec
+	#endif
 }
 
 int main(void){
 	setup();
-	register_handler(REQ_CONNECTION, test_function);
-	register_handler(REQ_SETTING, test_function_2);
-	register_handler(REQ_STATUS, return_status);
+	SCH_Start();
 	
-	sei();
-	
-	//init_USART(DEFAULT_UBRR);
-	//init_adc();
-
-
-    while (1);
+    while (1){
+		SCH_Dispatch_Tasks();
+	}
 }
 
