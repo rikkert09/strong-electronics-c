768cbe03ec1cd45dc9514a7a2dab5b66eec7ab0b
Joep Buruma <j.buruma@st.hanze.nl>
2017-11-09 16:18:13 +0100

    Add handlers for serial communication

--- strong-electronics-c/main.c
+++ strong-electronics-c/main.c
@@ -2,6 +2,7 @@
 #define F_CPU 16000000
 
 #include <stdint.h>
+#include <string.h>
 
 #include "rs232.h"
 #include "besturings_eenheid_prot.h"
@@ -12,6 +13,16 @@
 #define LIGHT_MOD
 
 uint16_t sensor_hist [2] = {0, 0};
+uint16_t setting_min_ext = 10;
+uint16_t setting_max_ext = 130;
+uint16_t sensor_trig_val = 150;
+uint16_t op_mode = OP_MODE_AUTO;
+uint16_t setting_ext_in_out = 0;
+uint16_t setting_ext_to_val = 0;
+
+uint8_t* device_name = "Dummy Name";
+
+uint8_t is_connected = 0;
 
 uint16_t get_temperature() {
 	uint8_t adc_value = get_adc_value(0);
@@ -35,6 +46,87 @@ void return_status(uint16_t data){
 	send_short_USART(avg_sensor, TRANSMIT_LITTLE_ENDIAN);
 }
 
+void handler_connection_request(uint16_t data){
+	is_connected = 1;
+	send_reply(ACK_CONNECTION, 0);
+}
+
+void handler_request_device_name(uint16_t data){
+	send_byte_USART(RET_DEVICE_NAME);
+	write_string(device_name);
+}
+
+void handler_update_device_name(uint16_t data){
+	uint8_t *new_name = read_string(new_name, data);
+	strcpy(device_name, new_name);
+	
+}
+
+void handler_request_setting(uint16_t data){
+	uint16_t ret_val = 0;
+	switch(data){
+		case SETTING_MIN_EXTEND:
+			ret_val = setting_min_ext;
+			break;
+		case SETTING_MAX_EXTEND:
+			ret_val = setting_max_ext;
+			break;
+		case SETTING_SENSOR_TRIG_VAL:
+			ret_val = sensor_trig_val;
+			break;
+		case SETTING_OP_MODE:
+			ret_val = op_mode;
+			break;
+		case SETTING_EXTEND_IN_OUT:
+			ret_val = setting_ext_in_out;
+			break;
+		case SETTING_EXTEND_TO_VAL:
+			ret_val = setting_ext_to_val;
+			break;
+	}
+	
+	send_reply(RET_SETTING, ret_val);
+}
+
+void handler_update_setting(uint16_t data){
+	uint8_t succes_failure = FAILURE;
+	uint8_t setting = data >> 8	;
+	uint8_t new_value = data;
+	switch(setting){
+		case SETTING_MIN_EXTEND:
+			if(new_value < setting_max_ext){
+				setting_min_ext = new_value;
+				succes_failure = SUCCES;
+			}			
+			break;
+		case SETTING_MAX_EXTEND:
+			if(new_value > setting_min_ext){
+				setting_max_ext = new_value;
+				succes_failure = SUCCES;
+			}			
+			break;
+		case SETTING_SENSOR_TRIG_VAL:
+			sensor_trig_val = new_value;
+			succes_failure = SUCCES;
+			break;
+		case SETTING_OP_MODE:
+			op_mode = new_value;
+			succes_failure = SUCCES;
+			break;
+		case SETTING_EXTEND_IN_OUT:
+			break;
+		case SETTING_EXTEND_TO_VAL:
+			break;
+	}
+
+	send_reply(ACK_UPD_SETTING, succes_failure);
+}
+
+void handler_request_disconnect(uint16_t data){
+	is_connected = 0;
+	send_reply(ACK_DISCONNECT, 0);
+}
+
 void update_status(){
 	sensor_hist[1] = sensor_hist[0];
 #ifdef LIGHT_MOD
@@ -50,16 +142,22 @@ void setup(){
 	//init_distance();
 	init_adc();
 	
+	register_handler(REQ_DEVICE_NAME, handler_request_device_name);
+	register_handler(REQ_CONNECTION, handler_connection_request);
+	register_handler(REQ_DISCONNECT, handler_request_disconnect);
 	register_handler(REQ_STATUS, return_status);
+	register_handler(REQ_SETTING, handler_request_setting);
+	register_handler(UPD_SETTING, handler_update_setting);
 	
-	SCH_Init_T1();	
+	SCH_Init_T1();
+		
 	SCH_Add_Task(handle_comms, 0, 1);
-	#ifdef LIGHT_MOD
-	SCH_Add_Task(update_status, 300, 300);				// if this is a light sensor, update every 30 sec
-	#endif
-	#ifdef TEMP_MOD
+#ifdef LIGHT_MOD
+	SCH_Add_Task(update_status, 250, 300);				// if this is a light sensor, update every 30 sec
+#endif
+#ifdef TEMP_MOD
 	SCH_Add_Task(update_status, 400, 400);				// if this is a temperature sensor, update every 40 sec
-	#endif
+#endif
 }
 
 int main(void){
